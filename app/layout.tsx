268311009510ce5d494b7de3e401f031ddc72908
rezadrian01 <ahmadadrian324@gmail.com>
2025-03-13 07:39:30 +0700

    feat: create send message feature, create download my cv feature, and change my projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -49,6 +50,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#35434E] text-slate-200 relative`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
